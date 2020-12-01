2def2b74a92aca7e63b474c9cb61c2b6ccb225b4
Matt Schleifer <Matthew.Schleifer@wisc.edu>
2020-11-30 22:18:38 -0600

    Apply some theming using tailwind config, nav bar tweaks, logo

---
+++ Fantasy/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                'fantasy-green': '#39B54A',
+                'gray-font': '#494949'
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
