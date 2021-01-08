0084c482e5b3bd27bb673c3bfb60473b78dc3605
Matt Schleifer <Matthew.Schleifer@wisc.edu>
2021-01-08 08:33:09 -0600

    Move to a custom version of the espn-fantasy-api package

--- Fantasy.API/Boxscore/index.js
+++ Fantasy.API/Boxscore/index.js
@@ -1,14 +1,8 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    //const name = (req.query.name || (req.body && req.body.name));
-    //const responseMessage = name
-    //    ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    //    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    context.log('JavaScript HTTP trigger function processed a request for Boxscore.');
 
     // importing ff node package
-    const { Client } = require('espn-fantasy-football-api/node');
+    const { Client } = require('../espn-fantasy-api.js');
     context.log('ESPNFantasyAPI imported.');
 
     const LEAGUE_ID = context.bindingData.leagueId; //551600, 1246423
