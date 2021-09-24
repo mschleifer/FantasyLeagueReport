af2cc217b0b99df575858b71a7265c2f809933c8
Matt Schleifer <Matthew.Schleifer@wisc.edu>
2021-09-24 15:40:37 -0500

    Add dark mode toggle

--- Fantasy/tailwind.config.js
+++ Fantasy/tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
     purge: [],
-    darkMode: false, // or 'media' or 'class'
+    darkMode: 'class', // false or 'media' or 'class'
     theme: {
         extend: {
             colors: {
                 'fantasy-green': '#39B54A',
-                'gray-font': '#494949'
+                'gray-font': '#494949',
+                'darkest-gray': '#121212',
+                'white-opacity-12': 'rgba(255,255,255,0.12)'
             }
         },
     },
