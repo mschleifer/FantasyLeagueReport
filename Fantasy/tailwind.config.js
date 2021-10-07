843e858102cd042d4e1ef4b7b34e961140d8ccb3
Matt Schleifer <Matthew.Schleifer@wisc.edu>
2021-10-07 18:24:51 -0500

    Add table striping for the season report

--- Fantasy/tailwind.config.js
+++ Fantasy/tailwind.config.js
@@ -7,12 +7,15 @@ module.exports = {
                 'fantasy-green': '#39B54A',
                 'gray-font': '#494949',
                 'darkest-gray': '#121212',
-                'white-opacity-12': 'rgba(255,255,255,0.12)'
+                'white-opacity-12': 'rgba(255,255,255,0.12)',
+                'white-opacity-16': 'rgba(255,255,255,0.16)'
             }
         },
     },
     variants: {
-        extend: {},
+        extend: {
+            backgroundColor: ['odd','even'],
+        }
     },
     plugins: [],
 }
