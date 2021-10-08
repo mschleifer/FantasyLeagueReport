9d57259fc574ae53e5535c4cad828b511e26ded8
Matt Schleifer <Matthew.Schleifer@wisc.edu>
2021-10-08 16:55:48 -0500

    Add gulp for css processing

---
+++ Fantasy/gulpfile.js
@@ -0,0 +1,32 @@
+﻿const gulp = require('gulp');
+const rename = require('gulp-rename');
+
+gulp.task('styles', () => {
+    const postcss = require('gulp-postcss');
+
+    return gulp.src('./Styles/fantasy.css')
+        .pipe(postcss([
+            require('precss'),
+            require('tailwindcss'),
+            require('autoprefixer')
+        ]))
+        .pipe(rename("app.css"))
+        .pipe(gulp.dest('./wwwroot/css/'));
+});
+
+gulp.task('styles-prod', () => {
+    const postcss = require('gulp-postcss');
+    const purgecss = require('gulp-purgecss');
+    const cleanCSS = require('gulp-clean-css');
+
+    return gulp.src('./Styles/fantasy.css')
+        .pipe(postcss([
+            require('precss'),
+            require('tailwindcss'),
+            require('autoprefixer')
+        ]))
+        .pipe(purgecss({ content: ['**/*.html', '**/*.razor'] }))
+        .pipe(rename("app.css"))
+        .pipe(cleanCSS({ level: 2 }))
+        .pipe(gulp.dest('./wwwroot/css/'));
+});
